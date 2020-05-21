9cd8c659786112687874e120e3b6964c917cb5a4
Moussa Iskounene <moussa.iskounene@gmail.com>
2020-05-22 01:55:02 +0200

    style(project): install styled-components package

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Moussa Iskounene" />
+    <Container>
+      <p>wsh</p>
+    </Container>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
